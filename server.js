55d877046a069dd1b80454b5ba54c0fc0014142c
Fernández Carillo, Miguel Ángel <cociocibil@ono.com>
2016-02-27 22:29:38 +0100

    Creamos un servidor web estático muy básico

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const http = require('http')
+const fs = require('fs')
 const port = process.env.PORT || 8080
 
 const server = http.createServer()
@@ -12,7 +13,8 @@ server.on('listening',onListening)
 server.listen(port)
 
 function onRequest(req, res){
-	res.end("Hola io.js")
+	let file = fs.readFileSync('public/index.html')
+	res.end(file)
 }
 
 function onListening(){
