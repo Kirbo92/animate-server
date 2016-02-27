5ffbdb6a9c8dc6e43a9b65508e1e0120b725c3d2
Fernández Carillo, Miguel Ángel <cociocibil@ono.com>
2016-02-27 23:04:58 +0100

    Refactor: Utilizar stream en 'fs'

--- server.js
+++ server.js
@@ -11,16 +11,18 @@ const server = http.createServer()
 server.on('request',onRequest)
 server.on('listening',onListening)
 
-
 server.listen(port)
 
 function onRequest(req, res){
 	let fileName = path.join(__dirname, 'public', 'index.html')
-	fs.readFile(fileName, function(err, file){
-		if (err){
-			return res.end(err.message)
-		}
-		res.end(file)
+
+	res.setHeader('Content-Type', 'text/html')
+	let rs = fs.createReadStream(fileName)
+
+	rs.pipe(res)
+
+	rs.on('error', function(err){
+		res.end(err.message)
 	})
 	
 }
