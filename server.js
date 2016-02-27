94a86ae2a24e9cd6dec6d20316b65319ba4b0b54
Fernández Carillo, Miguel Ángel <cociocibil@ono.com>
2016-02-28 00:45:06 +0100

    Servir index y app.js sin problemas

--- server.js
+++ server.js
@@ -14,17 +14,46 @@ server.on('listening',onListening)
 server.listen(port)
 
 function onRequest(req, res){
-	let fileName = path.join(__dirname, 'public', 'index.html')
+	let uri = req.url
+
+	if (uri.startsWith('/index') || uri === '/'){
+		return serveIndex(res)
+	}
+
+	if (uri === '/app.js'){
+		return serveApp(res)
+	}
+
+	res.statusCode = 404
+	res.end(`404 not found: ${uri}`)
+}
+
+function serveIndex(res){
+	let index = path.join(__dirname, 'public', 'index.html')
 
 	res.setHeader('Content-Type', 'text/html')
-	let rs = fs.createReadStream(fileName)
+	let rs = fs.createReadStream(index)
 
 	rs.pipe(res)
 
 	rs.on('error', function(err){
+		res.setHeader('Content-Type', 'text/plain')	
 		res.end(err.message)
 	})
-	
+}
+
+function serveApp(res){
+	let app = path.join(__dirname, 'public', 'app.js')
+
+	res.setHeader('Content-Type', 'text/javascript')
+	let rs = fs.createReadStream(app)
+
+	rs.pipe(res)
+
+	rs.on('error', function(err){
+		res.setHeader('Content-Type', 'text/plain')	
+		res.end(err.message)
+	})
 }
 
 function onListening(){
